11f2ee4c0733279562b906edb9a96cb4c923557d
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-15 15:01:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main_container'>
+      <h1 className='text-4xl'>PDP_Shop</h1>
+      
+    </div>
+  )
+}
+
+export default App
